bab8c147ac1325375d465bd624af8de8eae998a3
Eyad Hazem <eyadhazem258@gmail.com>
2026-05-03 21:38:27 +0300

    Minor updates in Bitonic Problem

--- BitonicArray.cpp
+++ BitonicArray.cpp
@@ -1,128 +1,127 @@
 #include <iostream>
+#include <vector>
+#include <string>
 using namespace std;
 
-int totalComparisons = 0;
-int totalSwaps = 0;
+class BitonicSorter {
+private:
+    int totalComparisons;
+    int totalSwaps;
 
-void bubbleSortAscending(int arr[], int start, int end)
-{
-	for (int i = start; i < end; i++)
-	{
-		for (int j = start; j < end - (i - start); j++)
-		{
-			totalComparisons++;
-			if (arr[j] > arr[j + 1])
-			{
-				swap(arr[j], arr[j + 1]);
-				totalSwaps++;
-			}
+    void bubbleSortAscending(vector<int>& arr, int start, int end) 
+    {
+        for (int i = start; i < end; i++) 
+        {
+            for (int j = start; j < end - (i - start); j++) 
+            {
+                totalComparisons++;
+                if (arr[j] > arr[j + 1]) 
+                {
+                    swap(arr[j], arr[j + 1]);
+                    totalSwaps++;
+                }
+            }
+        }
+    }
 
-		}
+    void selectionSortDescending(vector<int>& arr, int start, int end) 
+    {
+        for (int i = start; i <= end; i++) 
+        {
+            int maxIndex = i;
+            for (int j = i + 1; j <= end; j++) 
+            {
+                totalComparisons++;
+                if (arr[j] > arr[maxIndex])
+                    maxIndex = j;
+            }
+            if (maxIndex != i) 
+            {
+                swap(arr[i], arr[maxIndex]);
+                totalSwaps++;
+            }
+        }
+    }
 
-	}
+public:
+    BitonicSorter() : totalComparisons(0), totalSwaps(0) {}
 
-}
+    void sort(vector<int>& arr) 
+    {
+        int mid = arr.size() / 2;
+        bubbleSortAscending(arr, 0, mid);
+        selectionSortDescending(arr, mid + 1, arr.size() - 1);
+    }
 
-void selectionSortDescending(int arr[], int start, int end)
-{
-	for (int i = start; i <= end; i++)
-	{
-		int maxIdx = i;
-		for (int j = i + 1; j <= end; j++)
-		{
-			totalComparisons++;
-			if (arr[j] > arr[maxIdx])
-			{
-				maxIdx = j;
-			}
+    int findBitonicPoint(const vector<int>& arr) {
+        int maxIndex = 0;
+        for (int i = 1; i < arr.size(); i++) 
+        {
+            if (arr[i] > arr[maxIndex])
+                maxIndex = i;
+        }
+        return maxIndex;
+    }
 
-		}
-		if (maxIdx != i)
-		{
-			swap(arr[i], arr[maxIdx]);
-			totalSwaps++;
-		}
-	}
-}
+    int linearSearch(const vector<int>& arr, int target) 
+    {
+        for (int i = 0; i < arr.size(); i++) 
+        {
+            if (arr[i] == target) 
+                return i;
+        }
+        return -1;
+    }
 
-void bitonicSort(int arr[], int n)
-{
-	int mid = n / 2;
+    void printArray(const vector<int>& arr) 
+    {
+        cout << "[ ";
+        for (int i = 0; i < arr.size(); i++) 
+        {
+            cout << arr[i];
+            if (i != arr.size() - 1) 
+                cout << ", ";
+        }
+        cout << " ]";
+    }
 
-	bubbleSortAscending(arr, 0, mid);
-	selectionSortDescending(arr, mid + 1, n - 1);
+    void runTestCase(vector<int> arr, int target, const string& description) {
+        totalComparisons = 0;
+        totalSwaps = 0;
 
-}
+        cout << "--------------------------------------------\n";
+        cout << "Test: " << description << "\n";
+        cout << "Original Array : "; printArray(arr); cout << "\n";
 
-int linearSearch(const int arr[], int n, int target)
-{
-	for (int i = 0; i < n; i++)
-	{
-		if (arr[i] == target)
-		{
-			return i;
-		}
+        sort(arr);
+        int bitonicIndex = findBitonicPoint(arr);
 
-	}
-	return -1;
-}
+        cout << "Bitonic Array  : "; printArray(arr);
+        cout << " (Bitonic point = " << arr[bitonicIndex]
+            << " at index " << bitonicIndex << ")\n";
+        cout << "Comparisons: " << totalComparisons << ", Swaps: " << totalSwaps << "\n";
 
-void printArray(const int arr[], int n) {
-	cout << "[ ";
-	for (int i = 0; i < n; i++)
-	{
-		cout << arr[i] << " ";
-		if (i != n - 1)
-			cout << ", ";
-	}
-	cout << "]";
-}
+        int resultIndex = linearSearch(arr, target);
+        if (resultIndex != -1)
+            cout << "Search Target " << target << ": Found at index " << resultIndex << "\n";
+        else
+            cout << "Search Target " << target << ": Not Found\n";
 
-void runTestCase(int arr[], int n, int target, const char* description)
-{
-	totalComparisons = 0;
-	totalSwaps = 0;
-
-	cout << "--------------------------------------------\n";
-	cout << "Test: " << description << "\n";
-	cout << "Original array: ";
-	printArray(arr, n);
-	cout << "\n";
-
-	bitonicSort(arr, n);
-
-	int mid = n / 2;
-
-	cout << "Bitonic Array : ";
-	printArray(arr, n);
-	cout << " (Bitonic point = " << arr[mid] << " at index " << mid << ")\n";
-	cout << "Comparisons: " << totalComparisons << ", Swaps: " << totalSwaps << "\n";
-
-	int result = linearSearch(arr, n, target);
-	if (result != -1)
-		cout << "Search Target " << target << ": Found at index " << result << "\n";
-	else
-		cout << "Search Target " << target << ": Not Founded \n";
-
-	cout << "\n";
-}
+        cout << "\n";
+    }
+};
 
 int main() {
+    BitonicSorter sorter;
 
-	int arr1[] = { 3, 1, 7, 12, 9, 6, 4 };
-	runTestCase(arr1, 7, 12, "Target at bitonic point (target=12)");
+    sorter.runTestCase({ 3, 1, 7, 12, 9, 6, 4 }, 12, "Target at bitonic point (target=12)");
+    sorter.runTestCase({ 5, 2, 8, 15, 11, 7, 1 }, 5, "Target in left half (target=5)");
+    sorter.runTestCase({ 4, 1, 9, 20, 14, 6, 3 }, 6, "Target in right half (target=6)");
+    sorter.runTestCase({ 10, 3, 7, 18, 13, 8, 2 }, 100, "Target not present (target=100)");
+    sorter.runTestCase({ 5, 3, 5, 9, 9, 3, 1 }, 5, "Duplicate elements (target=5)");
+    sorter.runTestCase({ 1, 2, 3, 4, 5, 6, 20 }, 20, "Largest at last index (target=20)");
 
-	int arr2[] = { 5, 2, 8, 15, 11, 7, 1 };
-	runTestCase(arr2, 7, 5, "Target in left half / ascending portion (target=5)");
 
-	int arr3[] = { 4, 1, 9, 20, 14, 6, 3 };
-	runTestCase(arr3, 7, 6, "Target in right half / descending portion (target=6)");
 
-	int arr4[] = { 10, 3, 7, 18, 13, 8, 2 };
-	runTestCase(arr4, 7, 100, "Target not present in array (target=100)");
-
-	int arr5[] = { 5, 3, 5, 9, 9, 3, 1 };
-	runTestCase(arr5, 7, 5, "Array with duplicate elements (target=5)");
-
-	return 0;
+    return 0;
 }
